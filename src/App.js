17cd466d168445c0a83e2c8a0825c7c8bfea4f2a
rimshafatima59 <91379746+rimshafatima59@users.noreply.github.com>
2022-08-31 03:22:43 +0500

    31-08-2022

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/common/heading/Header"
 import { BrowserRouter as Router,  Switch, Route } from "react-router-dom";
 import './App.css'
 import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 const App = () => {
   return (
@@ -12,7 +13,7 @@ const App = () => {
         <Header />
         <Switch>
           <Route path="/" exact component={Home} />
-          
+          <Route path="/about" exact component={About} />
           
         </Switch>
       </Router>
---
+++ src/components/about/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Back from '../common/back/Back'
+
+const About = () => {
+  return (
+    <>
+        <h1>About</h1>
+        <Back title = "About Us"/>
+    </>
+  )
+}
+
+export default About
