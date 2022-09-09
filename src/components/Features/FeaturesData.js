3be151a16d248050699782ab9269785a43390bbb
rimshafatima59 <91379746+rimshafatima59@users.noreply.github.com>
2022-09-09 23:55:10 +0500

    features section

---
+++ src/components/Features/FeaturesData.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+      id: 1,
+      image: require("../../assets/images/Features/INBOUND-CALL.avif") ,
+      title: "INBOUND CALLS",
+      desc:"Ensure a positive and efficient customer experience with every call by letting our team of professionals handle customer enquiries, where they provide immediate responses and comprehensive support. ",
+    },
+    {
+      id: 2,
+      image: require("../../assets/images/Features/OUTBOND-CALL.avif"),
+      title: "OUTBOUND CALLS",
+      desc: "We help you engage your customers the right way, reaching out to provide proactive support, managing feedback and improving sales, leaving you with the unbridled freedom to achieve your full potential. ",
+    },
+    {
+      id: 3, 
+      image: require("../../assets/images/Features/CHAT-SUPPORT.avif"),
+      title: "CHAT SUPPORT",
+      desc: "Experience lightning-fast and hassle-free customer service support with our live chat feature, where we provide a cross-platform human interface for customers to navigate online effortlessly, helping you deliver fully on your customer service promises.",
+    },
+    {
+      id: 4,
+      image: require("../../assets/images/Features/EMAIL-SUPPORT.avif"),
+      title: "EMAIL SUPPORT",
+      desc: "Building strong and lasting relationships with your customers through our email support service, where we comprehensively and efficiently manage concerns, enquiries and outreach, increasing customer loyalty and satisfaction.",
+    },
+    {
+      id: 5,
+      image: require("../../assets/images/Features/MARKET-SURVEY.avif"),
+      title: "MARKET SURVEY",
+      desc: "Lead market trends instead of following them: our team conducts swift, on-demand primary and secondary market research that will ensure you’re always a step ahead in the right direction.",
+    },
+    {
+      id: 6,
+      image: require("../../assets/images/Features/OMNI-CHANNEL.avif"),
+      title: "OMNI CHANNEL",
+      desc: "With our omni-channel support marked by its signature flexibility and responsiveness, your customers can always expect a simple, seamlessly integrated experience across multiple platforms like voice, chat, email and all social media messaging services.",
+    },
+  ]
+  export default data
